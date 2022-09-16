25f568fbaa3b4f2944dc918506a2de020faac281
NadiaMonzon <nadiamonque99@gmail.com>
2022-09-16 18:43:02 +0100

    Try to fetch

--- src/services/getDataApi.js
+++ src/services/getDataApi.js
@@ -1,24 +1,24 @@
-const getDataApi = () =>{
-    return fetch('https://hp-api.herokuapp.com/api/characters')
-    .then((response)=>response.json())
-    .then((data)=>{
-        const cleanData= data.map((character, index)=>{
-            return {
-                name: character.name,
-                species: character.species,
-                gender: character.gender,
-                house: character.house,
-                alive: character.alive,
-                image: character.image,
-                othername: character.alternate_names,
-                student: character.hogwartsStudent,
-                id: index,
-            }
-            
-        });
-        return cleanData;
+const getDataApi = () => {
+    return fetch('http://hp-api.herokuapp.com/api/characters')
+        .then((response) => response.json())
+        .then((data) => {
+            const cleanData = data.map((character, index) => {
+                return {
+                    name: character.name,
+                    species: character.species,
+                    gender: character.gender,
+                    house: character.house,
+                    alive: character.alive,
+                    image: character.image,
+                    othername: character.alternate_names,
+                    student: character.hogwartsStudent,
+                    id: index,
+                }
 
-    })
+            });
+            return cleanData;
+
+        })
 }
 
 export default getDataApi;

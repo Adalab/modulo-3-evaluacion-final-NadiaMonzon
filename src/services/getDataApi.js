8ad05948065aa7674c7004c504da43a0ecdfacbe
NadiaMonzon <nadiamonque99@gmail.com>
2022-09-16 16:13:00 +0100

    Improve security

--- src/services/getDataApi.js
+++ src/services/getDataApi.js
@@ -1,5 +1,5 @@
 const getDataApi = () =>{
-    return fetch('http://hp-api.herokuapp.com/api/characters')
+    return fetch('https://hp-api.herokuapp.com/api/characters')
     .then((response)=>response.json())
     .then((data)=>{
         const cleanData= data.map((character, index)=>{
